3dd98e8b8ff7420f57092ba8f204da571ea82bf2
Sarthak3003 <sarthakcr007@gmail.com>
2022-04-11 09:31:35 +0530

    views

--- todo/views.py
+++ todo/views.py
@@ -5,12 +5,12 @@ from .forms import TodoForm
 
 #showing ordered list
 def index(request):
-    todo_list = Todo.objects.order_by('id')                 #creates ordered list
+    todo_list = Todo.objects.order_by('id')                     #creates ordered list
     form = TodoForm()
-    context = {'todo_list' : todo_list, 'form' : form}      #passes context to index.html
+    context = {'todo_list' : todo_list, 'form' : form}          #passes context to index.html
     return render(request, 'todo/index.html', context)
 
-@require_POST                                           #only accepts POST requests
+@require_POST                                                   #only accepts POST requests
 def addTodo(request):
     form = TodoForm(request.POST)
 
@@ -19,17 +19,17 @@ def addTodo(request):
         new_todo.save()
     return redirect('index')
 
-def completeTodo(request, todo_id):                     #manage completed tasks ;)
+def completeTodo(request, todo_id):                             #manage completed tasks ;)
     todo = Todo.objects.get(pk=todo_id)
     todo.complete = True
     todo.save()
     return redirect('index')
 
-def deleteCompleted(request):                           #this is to manage deleted task
+def deleteCompleted(request):                                   #this is to manage deleted task
     Todo.objects.filter(complete__exact=True).delete()
     return redirect('index')
 
-def deleteAll(request):                                 #when you want to delete all task 
+def deleteAll(request):                                         #when you want to delete all task 
     Todo.objects.all().delete()
 
     return redirect('index')
